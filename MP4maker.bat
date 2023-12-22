76de467cd517c4562b90f1c5a0e78d44d8a73825
Arthur Melgoza <38125347+arthurmv@users.noreply.github.com>
2023-12-21 19:47:31 -0800

    Update MP4maker.bat
    H265 update and temp. NVIDIA transcoding disabled.

--- MP4maker.bat
+++ MP4maker.bat
@@ -1,20 +1,36 @@
 @ECHO OFF
 TITLE MP4maker
+
 CD /D %~dp1
-REM CHECK GPU
-SET "GPU1=-i"
-SET "GPU2=-vcodec h264 -preset veryslow -crf 12 -tune film"
->_tmp (wmic PATH Win32_VideoController GET AdapterCompatibility)
-FIND /I "NVIDIA" < _tmp > NUL
-IF %errorlevel%==0 SET "GPU1=-hwaccel cuda -hwaccel_output_format cuda -i" & SET "GPU2=-vcodec h264_nvenc -crf 12"
-DEL _tmp
-REM CHECK AUDIO
+
+SET "GPU=-vcodec libx265"
+
+REM ---GPU CHECKING---
+REM >_tmp (wmic PATH Win32_VideoController GET AdapterCompatibility)
+REM FIND /I "NVIDIA" < _tmp > NUL
+REM IF %errorlevel%==0 SET "GPU=-vcodec h264_nvenc -preset:v p7 -tune:v grain -rc:v vbr -cq:v 12 -b:v 0 -profile:v high"
+REM DEL _tmp
+
+REM ---AUDIO CHECKING---
 SET AUDIO=X
 >_tmp (mediainfo --Output="Audio;%%Format%%" "%~nx1")
 SET /P AUDIO=<_tmp
 DEL _tmp
+
+REM ---VIDEO BITRATE CHECKING---
+>_tmp (mediainfo --Output="Video;%%BitRate%%" "%~nx1")
+SET VBA=1
+SET /P VBA=<_tmp
+DEL _tmp
+SET /A VBB=%VBA%/1000
+SET "VB=-b:v %VBB%k"
+IF %VBB%==0 SET "VB=-crf 18"
+
+REM --- AUDIO CHECKING---
 IF %AUDIO%==AAC SET "AB=-c:a copy" & GOTO CHECK-VIDEO
 IF %AUDIO%==FLAC SET "AB=-c:a copy" & GOTO CHECK-VIDEO
+
+REM ---AUDIO BITRATE CHECKING---
 >_tmp (mediainfo --Output="Audio;%%BitRate%%" "%~nx1")
 SET ABBB=X
 SET /P ABBB=<_tmp
@@ -22,17 +38,19 @@ DEL _tmp
 SET /A ABB=%ABBB%/1000
 SET "AB=-b:a %ABB%k"
 IF %ABB% GEQ 320 SET "AB=-b:a 320k"
+
 : CHECK-VIDEO
 SET VIDEO=X
 >_tmp (mediainfo --Inform="Video;%%ScanType%%" "%~nx1")
 SET /P VIDEO=<_tmp
 DEL _tmp
 IF %VIDEO%==Interlaced GOTO INTERLACED
+
 : TRANSCODE
-ffmpeg-bar %GPU1% "%~nx1" %GPU2% -acodec aac %AB% -scodec mov_text "%~n1.mp4"
+ffmpeg-bar -i "%~nx1" %GPU% %VB% -acodec aac %AB% -scodec mov_text "%~n1.mp4"
 GOTO FINISH
 : INTERLACED
-ffmpeg-bar %GPU1% "%~nx1" %GPU2% -filter:v bwdif=mode=send_field:parity=auto:deint=all -acodec aac %AB% -scodec mov_text "%~n1.mp4"
+ffmpeg-bar -i "%~nx1" %GPU% %VB% -filter:v bwdif=mode=send_field:parity=auto:deint=all -acodec aac %AB% -scodec mov_text "%~n1.mp4"
 : FINISH
 PAUSE
 EXIT
